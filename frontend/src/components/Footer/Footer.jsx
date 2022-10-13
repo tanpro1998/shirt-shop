f21003c58e42196508bc418e85015479091c74c2
tanpro1998 <davidtanpro1998@gmail.com>
2022-10-13 13:54:06 +0700

    fix footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -14,57 +14,54 @@ const Footer = () => {
       <div className=" bg-black color-white container p-5">
         <Grid col={4} mdCol={4} smCol={2} gap={10}>
           <div className="footer">
-          <div>
-            <div className="footer__title">Tổng đài hỗ trợ</div>
-            <div className="footer__content">
+            <div className="footer__contact">
+              <div className="footer__title">Tổng đài hỗ trợ</div>
+              <div className="footer__content">
+                <p>
+                  Liên hệ đặt hàng <strong>0964903991</strong>
+                </p>
+                <p>
+                  Thắc mắc đơn hàng <strong>0964903991</strong>
+                </p>
+                <p>
+                  Góp ý, khiếu nại <strong>0964903991</strong>
+                </p>
+              </div>
+            </div>
+            <div className="footer__about">
               <p>
-                Liên hệ đặt hàng <strong>0964903991</strong>
+                <Link to="/">
+                  <span
+                    style={{
+                      fontSize: "40px",
+                      fontWeight: "bold",
+                      color: "white",
+                    }}
+                  >
+                    FLASH
+                  </span>
+                </Link>
               </p>
               <p>
-                Thắc mắc đơn hàng <strong>0964903991</strong>
-              </p>
-              <p>
-                Góp ý, khiếu nại <strong>0964903991</strong>
+                Hướng đến mục tiêu mang lại niềm vui ăn mặc mới mỗi ngày cho
+                hàng triệu người tiêu dùng Việt. Hãy cùng Flash hướng đến một
+                cuộc sống năng động, tích cực hơn.
               </p>
             </div>
-          </div>
-          <div
-            className="footer__about"
-            // style={{ transform: "translateX(100%)" }}
-          >
-            <p>
-              <Link to="/">
-                <span
-                  style={{
-                    fontSize: "40px",
-                    fontWeight: "bold",
-                    color: "white",
-                  }}
-                >
-                  FLASH
-                </span>
-              </Link>
-            </p>
-            <p>
-              Hướng đến mục tiêu mang lại niềm vui ăn mặc mới mỗi ngày cho hàng
-              triệu người tiêu dùng Việt. Hãy cùng Flash hướng đến một cuộc sống
-              năng động, tích cực hơn.
-            </p>
-          </div>
-          <div className="footer__link">
-            <a href="https://www.facebook.com/tan.duongtranthanh/">
-              <FacebookOutlined className="icon" />
-            </a>
-            <a href="https://www.instagram.com/tan.divad/">
-              <InstagramOutlined className="icon" />
-            </a>
-            <a href="https://twitter.com/Tan_david1661">
-              <TwitterOutlined className="icon" />
-            </a>
-            <a href="https://www.youtube.com/c/MixiGamingofficial">
-              <YoutubeOutlined className="icon" />
-            </a>
-          </div>
+            <div className="footer__link">
+              <a href="https://www.facebook.com/tan.duongtranthanh/">
+                <FacebookOutlined className="icon" />
+              </a>
+              <a href="https://www.instagram.com/tan.divad/">
+                <InstagramOutlined className="icon" />
+              </a>
+              <a href="https://twitter.com/Tan_david1661">
+                <TwitterOutlined className="icon" />
+              </a>
+              <a href="https://www.youtube.com/c/MixiGamingofficial">
+                <YoutubeOutlined className="icon" />
+              </a>
+            </div>
           </div>
         </Grid>
       </div>
